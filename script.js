21a88011ee8c04ff96d769205e568941d1b11771
linbeifong41 <lareenhammad2@gmail.com>
2025-06-18 16:07:56 +0300

    updated css, updated html

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.getElementsByClassName("login-card").addEventslistener("sumbit", function(event)) { 
-    event.preventDefault(); // stop the from from refreshing the page
-    const username= document.getElementsByClassName("username").value;
-    const password= document.getElementsByClassName("password").value;
+document.querySelector(".login-card").addEventlistener("sumbit", function(event) {
+    event.preventDefault(); 
+    const username= document.querySelector("username").value;
+    const password= document.querySelector("password").value;
 
     // fake login check
 if (username === "user" && password === "1234") {
-    localStorage.setItem("isLOggedIn", "true");
-    Window.location.href = "dashboard.html";  // Go to main web 
+    localStorage.setItem("isLoggedIn", "true");
+    Window.location.href = "dashboard.html"; 
 } else {
     alert("invalid login.");
 }
+
 });
